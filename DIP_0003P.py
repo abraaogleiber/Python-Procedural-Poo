ccfc199decc713892d0899a7201f088ffd8473e3
Abraão Gleiber <abraaocursos2019@gmail.com>
2021-03-26 22:48:27 -0300

    DIP_0003P

---
+++ DIP_0003P.py
@@ -0,0 +1,7 @@
+
+#__________________ Script Python - versão 3.8 _____________________#
+#  Autor |> Abraão A.da Silva
+#  Data |> 05 de Março de 2021
+#  Paradigma |> Procedural
+#  Objetivo |> Somando dois números
+#___________________________________________________________________#
